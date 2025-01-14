40c8c5d2d90266b04207181a9b110560f477be0f
Anjali Rawat <anjalirawat050805@gmail.com>
2025-01-14 20:23:32 +0530

    new

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
-const admin = require('./firebaseAdmin');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
